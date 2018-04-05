a2fcaf858cc1ed222c50aaa01d450840b2bc21f4
GFBryson <gillianfbryson@gmail.com>
2018-04-05 15:39:25 -0700

    update

---
+++ py3Server/resp2.py
@@ -0,0 +1,26 @@
+
+def resp_hello():
+	hello=[]
+	hello.append('Hey there "{usr}"! how\'s it going?')
+	hello.append('Hi "{usr}" :grinning:')
+	hello.append('Hello "{usr}", whats up?')
+	hello.append('Hey "{usr}" :wave:,\nHow are you doing?')
+	hello.append('Hi "{usr}"!\nI\'m StrugBot, and I\'m super happy to be here!!')
+	hello.append('Sup "{usr}"!')
+	hello.append('Hi "{usr}",\nwhat can I do for you?')
+	hello.append('WAZZZZUUUUUUUUUUUUUUP "{usr}"')	
+
+	return hello
+
+def resp_trex():
+	return"I am a T-Rex!:t-rex:\nI have a BIG head and little arms,\nRAWWWRRRRR!!"
+
+def resp_date():
+	return 'the date is: '
+
+def resp_time():
+	return 'the time is: '
+
+
+	
+
