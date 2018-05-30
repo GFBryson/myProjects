1903bfe1b07971232f5a35ef28f70782ca6ad6c8
GFBryson <gillianfbryson@gmail.com>
2018-05-30 19:16:57 +0000

    adding file to start making bot work with integrations

---
+++ py3Server/strugBotIntegrations.py
@@ -0,0 +1,4 @@
+import math
+import datetime
+
+#plan is to make strugbot have integraions which allow for more intuative use instead of having users needing to =know the exact formatting needed for servioces
